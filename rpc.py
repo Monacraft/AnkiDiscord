cf29c34e2989ca57d4ce29ec117b573e05b4d62c
Johann <johann.schumann0@gmail.com>
2023-02-12 19:46:06 +0900

    Log debug instead of error when file doesn't exist
    When discord is not opened and that the discord files doesn't exist, log a debug message instead of an error, to avoid anki opening an error window.

--- rpc.py
+++ rpc.py
@@ -148,7 +148,7 @@ class WinDiscordIpcClient(DiscordIpcClient):
             try:
                 self._f = open(path, "w+b")
             except OSError as e:
-                logger.error("failed to open {!r}: {}".format(path, e))
+                logger.debug("failed to open {!r}: {}".format(path, e))
             else:
                 break
         else:
@@ -180,7 +180,7 @@ class UnixDiscordIpcClient(DiscordIpcClient):
             try:
                 self._sock.connect(path)
             except OSError as e:
-                logger.error("failed to open {!r}: {}".format(path, e))
+                logger.debug("failed to open {!r}: {}".format(path, e))
             else:
                 break
         else:
